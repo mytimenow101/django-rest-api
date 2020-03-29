f51a66419804f55c4370152e75cbda4b193aedef
p.r <mytimenow@gmail.com>
2020-03-29 01:37:01 -0400

    added urls to the api

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.response import Response
 # Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+
+        an_apiview = [
+        'Uses HTTP methods as function (get, post, patch, put, delete)',
+        'Is similiar to a traditional Django View',
+        'is mapped manually to URLS '
+        ]
+
+        return Response({'message' : 'hello!', 'an_apiview' : an_apiview})
